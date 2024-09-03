1cbd1a7aa4ed2d26249c337bf1a9edc56afebae5
Oksana ogay <example@email.com>
2024-09-03 17:05:51 +0500

    Craete constants

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Vladimir on 03/09/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+}
